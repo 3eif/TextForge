13de42c59f576bd3cab8aa98a52f7d966e778a46
Tetra <seifaziz10@gmail.com>
2020-04-28 10:47:22 -0700

    Update textforge.lua

--- textforge.lua
+++ textforge.lua
@@ -1,4 +1,4 @@
-local text = "Sample Text`0123456789`.!?`"..os.date("%x");
+local text = "So basically, I'm monkey`";
 
 local characterSpacing = 6;
 local lineSpacing = 1.5;
@@ -7,9 +7,9 @@ local lineSpacing = 1.5;
   Use ` to create a new line
 
   All Supported Characters:
-    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    ., !, ?
+    a b c d e f g h i j k l m n o p q r s t u v w x y z
+    0 1 2 3 4 5 6 7 8 9
+    . ! ?
 --]=====]
 
 forge.clear();
